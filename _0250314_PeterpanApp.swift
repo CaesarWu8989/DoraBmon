19d2288bd6c063f8cc7916fe7d4b192a8361bbf3
CaesarWu8989 <caesar@aatp.com.tw>
2025-03-16 14:12:38 +0800

    Add files via upload

---
+++ _0250314_PeterpanApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0250314_PeterpanApp.swift
+//  20250314 Peterpan
+//
+//  Created by Caesar Wu on 2025/3/14.
+//
+
+import SwiftUI
+
+@main
+struct _0250314_PeterpanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
