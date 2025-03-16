a21f59b75173d69c2d3984326f52e795001bc755
CaesarWu8989 <caesar@aatp.com.tw>
2025-03-16 14:13:51 +0800

    Add files via upload

---
+++ _0250307HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  _0250307HW1App.swift
+//  20250307HW1
+//
+//  Created by Caesar Wu on 2025/3/7.
+//
+
+import SwiftUI
+
+@main
+struct _0250307HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
